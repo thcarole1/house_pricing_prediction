de1e526e51c8e47fac4f3c2c22bc14000dae1b6b
Thierry CAROLE <thcarole1@hotmail.com>
2024-12-21 12:48:23 +0100

    20241221_1248_backup

--- house_pricing_prediction_package_folder/api.py
+++ house_pricing_prediction_package_folder/api.py
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'New project': 'This is the first app of my new project !'}
+
+
+@app.post("/test")
+def essai(longitude : float,
+          latitude : float,
+          housing_median_age : int,
+          total_rooms : int,
+          total_bedrooms : int,
+          population : int,
+          households : int,
+          median_income : float,
+          ocean_proximity : str):
+    return longitude, latitude, housing_median_age,\
+          total_rooms, total_bedrooms,population,\
+          households, median_income, ocean_proximity
